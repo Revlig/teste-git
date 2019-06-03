1832ea04cce5eb35d03de3e0503603fc465bb716
Revlig <gilver.souza@clickbus.com>
2019-06-03 13:23:29 -0300

    adicionada funcao de subtracao

--- calc.py
+++ calc.py
@@ -1,8 +1,14 @@
+from operator import sub
 from operator import add
 
 def soma(a, b):
     return add(a + b)
 
 
+<<<<<<< HEAD
 def div(a, b):
     return a/b
+=======
+def sub(a, b):
+    return sub(a, b)
+>>>>>>> adicionada funcao de subtracao
